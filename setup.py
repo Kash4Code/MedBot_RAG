fee124fd710a72606932104f02a66ed5a4782ae8
Kashinath <rpkashinath@gmail.com>
2025-10-20 10:17:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Kashinath R P",
+    author_email="rpkashinath@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
